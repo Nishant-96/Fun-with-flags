bf56a2d83a682521544a4082b1e5b5b972b524c7
Nishant Pratihast <87017414+Nishant-96@users.noreply.github.com>
2021-07-27 01:29:15 +0530

    Update: app functionality done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
+import React, { useState } from "react";
+
 import "./styles.css";
 
+const flagDictionary = {
+  "🇮🇳": "India",
+  "🇮🇴": "British Indian Ocean Territory",
+  "🇮🇶": "Iraq",
+  "🇮🇷": "Iran",
+  "🇮🇸": "Iceland",
+  "🇮🇹": "Italy",
+  "🇯🇪": "Jersey",
+  "🇯🇲": "Jamaica",
+  "🇯🇴": "Jordan",
+  "🇯🇵": "Japan",
+  "🇰🇪": "Kenya",
+  "🇰🇬": "Kyrgyzstan",
+  "🇰🇭": "Cambodia",
+  "🇰🇮": "Kiribati",
+  "🇰🇲": "Comoros",
+  "🇰🇵": "North Korea",
+  "🇰🇷": "South Korea",
+  "🇰🇼": "Kuwait"
+};
+// 🇳🇷
+
+var flagsWeKnow = Object.keys(flagDictionary);
+
 export default function App() {
+  const [countryName, setCountryName] = useState("");
+
+  function changeHandler(event) {
+    var userInput = event.target.value;
+    var countryName = flagDictionary[userInput];
+
+    if (countryName === undefined) {
+      countryName = "We don't have this in our database";
+    }
+    setCountryName(countryName);
+  }
+
+  function clickHandler(flags) {
+    var countryName = flagDictionary[flags];
+    setCountryName(countryName);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Fun With Flags</h1>
+      <input
+        onChange={changeHandler}
+        placeholder="enter flag icon or click on below ones"
+      ></input>
+      <h2>{countryName}</h2>
+
+      {flagsWeKnow.map((flags) => {
+        return (
+          <span
+            onClick={() => clickHandler(flags)}
+            style={{
+              fontSize: "2rem",
+              padding: "0.5rem 1rem",
+              cursor: "pointer"
+            }}
+            key={flags}
+          >
+            {flags}
+            {"  "}
+          </span>
+        );
+      })}
     </div>
   );
 }
